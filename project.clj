9d4c61a1ac164a0422a8a8e768a912fe42241c30
Jan Krueger <jan.krueger@gmail.com>
2013-05-17 15:28:05 +0000

    Added some prototyping code

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject garamond "0.1.0-SNAPSHOT"
+  :description      "A collection of tools to document your github projects"
+  :url              "https://github.com/contentjon/garamond"
+  :license          {:name         "GPL"
+                     :url          "http://www.gnu.org/licenses/gpl.html"
+                     :distribution :repo}
+  :min-lein-version "2.0.0"
+  :plugins          [[lein-cljsbuild "0.3.1"]]
+  :dependencies     [[contentjon/kit.cljs "0.1.0"]]
+  :profiles         {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                          [mocha-latte "0.1.0"]]}}
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src"],
+     :id "dev",
+     :compiler
+     {:pretty-print  true,
+      :output-to     "garamond.js",
+      :optimizations :simple
+      :target        :nodejs}}]})
+
