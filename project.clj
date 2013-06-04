504ac2bc89ebe811cc1061f338b4da133192c00f
Jan Krueger <jan.krueger@gateprotect.com>
2013-06-04 16:58:33 +0200

    Use latest cljs

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject garamond "0.1.0-SNAPSHOT"
   :description      "A documentation generator framework with a focus on strong github support"
   :min-lein-version "2.0.0"
-  :plugins          [[lein-cljsbuild "0.3.1"]]
+  :plugins          [[lein-cljsbuild "0.3.2"]]
   :profiles         
   {:dev
    {:dependencies [[contentjon/kit.cljs "0.2.0-SNAPSHOT"]
